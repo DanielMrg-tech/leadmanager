f69a4faf2a8ea02ca9a3876962e5ef8776766d22
DanielMrg-tech <margineandaniel99@gmail.com>
2026-03-11 23:33:02 +0200

    Add new functionalities

---
+++ frontend/src/components/leads/Leads.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Leads extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Leads List</h2>
+      </div>
+    )
+  }
+}
+
+export default Leads
